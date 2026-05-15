458777eb7e3dda5da23a817c28f48e9a872744b0
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-05-15 23:43:47 +0530

    Day 5 - Navbar,Footer and Home page ui is Completed with redux store,slice is Done!!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to ShopNest!</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
